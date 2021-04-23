3a1d394afd31de66b882718f89805d7c1de20f6b
andrem91 <andrem166@gmail.com>
2021-04-23 20:14:38 -0300

    Hello World!

---
+++ logicaDeProgramacao/src/estruturaSequencial/Exercicio01.java
@@ -0,0 +1,14 @@
+/*
+* Faça um Programa que mostre a mensagem "Olá, mundo!" na tela.
+ */
+
+package estruturaSequencial;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+		
+	}
+
+}
